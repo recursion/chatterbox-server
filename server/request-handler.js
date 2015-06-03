60fc5f52e09f2491f0691ba0ee3555c0ccdbb58c
Michael Symmes <msymmes@gmail.com>
2015-06-02 23:08:27 -0400

    Further implemented express migration

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,83 +1,33 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var uuid = require('node-uuid');
 
 var messages = [];
 
-var returnMessages = function(request, response) {
-  constructHeader(response);
+var returnMessages = function(request, response, roomname) {
   var results = {};
-  results.results = messages;
+  var returnMessages;
+  if (roomname) {
+    returnMessages = getMessagesByRoom(roomname);
+  } else {
+    returnMessages = messages;
+  }
+  constructHeader(response);
+  results.results = returnMessages;
   response.end(JSON.stringify(results));
 };
 
-var requestHandler = function(request, response) {
-
-  //console.log("Serving request type " + request.method + " for url " + request.url);
-
-  // regex for matching roomnames
-  var re = /\/classes\/.*$/g;
-
-  // respond to OPTIONS requests
-  if (request.method === 'OPTIONS'){
-    constructHeader(response);
-    response.end();
-  }
-
-  // Routes Processing
-  if (request.url === '/classes/messages' ){
-    if (request.method === 'GET'){
-      constructHeader(response);
-      var results = {};
-      results.results = messages;
-      response.end(JSON.stringify(results));
-
-    } else if (request.method === 'POST'){
-      postBuilder(request, response);
+var getMessagesByRoom = function(roomname) {
+  var roomMessages = [];
+  for (var i =0; i < messages.length; i++){
+    if (messages[i].roomname === roomname){
+      roomMessages.push(messages[i]);
     }
-  } else if (re.test(request.url)) {
-    var path = request.url;
-    var roomname = path.split('/')[path.length - 1];
-
-    if (request.method === 'GET') {
-      constructHeader(response);
-
-      // gather all messages with this roomname
-      // and send tyhem back to the client
-      var results = {};
-      var roomMessages = [];
-      for (var i =0; i < messages.length; i++){
-        if (messages[i].roomname === roomname){
-          roomMessages.push(messages[i]);
-        }
-      }
-      results.results = roomMessages;
-      response.end(JSON.stringify(results));
-
-    } else if (request.method === 'POST') {
-      postBuilder(request, response);
-    }
-  } // if url does not match any of the above, return an 404 error header message
-  else {
-    constructHeader(response, 404);
-    response.end();
   }
+  return roomMessages;
 };
 
-
 // Gather POST data and pass it to messageBuilder
-var postBuilder = function(request, response) {
+var postBuilder = function(request, response, roomname) {
+  roomname = roomname || '';
   constructHeader(response, 201);
   var body = '';
   request.on('data', function(chunk){
@@ -121,4 +71,61 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 // Seconds.
 };
 
-module.exports.requestHandler = requestHandler;
+/// EXPORTS
+exports.returnMessages = returnMessages;
+exports.postBuilder = postBuilder;
+//exports.requestHandler = requestHandler;
+// var requestHandler = function(request, response) {
+
+  //console.log("Serving request type " + request.method + " for url " + request.url);
+
+  // regex for matching roomnames
+//   var re = /\/classes\/.*$/g;
+
+//   // respond to OPTIONS requests
+//   if (request.method === 'OPTIONS'){
+//     constructHeader(response);
+//     response.end();
+//   }
+
+//   // Routes Processing
+//   if (request.url === '/classes/messages' ){
+//     if (request.method === 'GET'){
+//       constructHeader(response);
+//       var results = {};
+//       results.results = messages;
+//       response.end(JSON.stringify(results));
+
+//     } else if (request.method === 'POST'){
+//       postBuilder(request, response);
+//     }
+//   } else if (re.test(request.url)) {
+//     var path = request.url;
+//     var roomname = path.split('/')[path.length - 1];
+
+//     if (request.method === 'GET') {
+//       constructHeader(response);
+
+//       // gather all messages with this roomname
+//       // and send tyhem back to the client
+//       var results = {};
+//       var roomMessages = [];
+//       for (var i =0; i < messages.length; i++){
+//         if (messages[i].roomname === roomname){
+//           roomMessages.push(messages[i]);
+//         }
+//       }
+//       results.results = roomMessages;
+//       response.end(JSON.stringify(results));
+
+//     } else if (request.method === 'POST') {
+//       postBuilder(request, response);
+//     }
+//   } // if url does not match any of the above, return an 404 error header message
+//   else {
+//     constructHeader(response, 404);
+//     response.end();
+//   }
+// };
+
+
