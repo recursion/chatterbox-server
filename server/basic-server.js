78fa96c16492c2841cafe5c1d3472aeb2f18dcf9
Michael Symmes <msymmes@gmail.com>
2015-06-02 21:45:10 -0400

    Started the migration to express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,30 +1,41 @@
 /* Import node's http module: */
 var http = require("http");
 var requestHandler = require("./request-handler").requestHandler;
+var express = require('express');
+var app = express();
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
 // normally already claimed by another server and/or not accessible
 // so we'll use a standard testing port like 3000, other common development
 // ports are 8080 and 1337.
-var port = 3000;
+
+app.use(express.static('client/client'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client/client/index.html');
+});
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Working on " + host + " and on " + port);
+});
+
 
 // For now, since you're running this server on your local machine,
 // we'll have it listen on the IP address 127.0.0.1, which is a
 // special address that always refers to localhost.
-var ip = "127.0.0.1";
-
-
-
 // We use node's http module to create a server.
 //
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(requestHandler);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+// var server = http.createServer(requestHandler);
+// console.log("Listening on http://" + ip + ":" + port);
+// server.listen(port, ip);
 
 // To start this server, run:
 //
--- server/request-handler.js
+++ server/request-handler.js
@@ -15,21 +15,14 @@ var uuid = require('node-uuid');
 
 var messages = [];
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+var returnMessages = function(request, response) {
+  constructHeader(response);
+  var results = {};
+  results.results = messages;
+  response.end(JSON.stringify(results));
+};
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+var requestHandler = function(request, response) {
 
   //console.log("Serving request type " + request.method + " for url " + request.url);
 
@@ -81,15 +74,7 @@ var requestHandler = function(request, response) {
     response.end();
   }
 };
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
 
-  //response.end("Hello, World!");
 
 // Gather POST data and pass it to messageBuilder
 var postBuilder = function(request, response) {
@@ -122,30 +107,13 @@ var messageBuilder = function (dataObj) {
 
 // creates header data, status code and content type for each request
 var constructHeader = function (response, status) {
-  // The outgoing status.
   status = status || 200;
   var statusCode = status;
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
   headers['Content-Type'] = "application/JSON";
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
   response.writeHead(statusCode, headers);
 };
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
+
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
